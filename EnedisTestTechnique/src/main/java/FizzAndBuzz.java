5dafaa407457d332e54071a81ecdb685c2e1d70d
younes <younes@192.168.1.34>
2017-03-07 20:31:10 +0100

    feat(service): add the case when it is divided by 5

--- EnedisTestTechnique/src/main/java/FizzAndBuzz.java
+++ EnedisTestTechnique/src/main/java/FizzAndBuzz.java
@@ -3,6 +3,7 @@ public class FizzAndBuzz {
 	
 	public static String dividibleBy(int number) {
 		if (number % 15 == 0) return "fizzbuzz";
+		else if (number % 5 == 0) return "buzz";
 		return Integer.toString(number); 
 	}
 }
