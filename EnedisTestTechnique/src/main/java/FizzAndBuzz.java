8fcfbaef2f4300c79c3d00722e26e2b3dec822eb
younes <younes@192.168.1.34>
2017-03-07 20:37:21 +0100

    feat(service): number devided by 3

--- EnedisTestTechnique/src/main/java/FizzAndBuzz.java
+++ EnedisTestTechnique/src/main/java/FizzAndBuzz.java
@@ -4,6 +4,7 @@ public class FizzAndBuzz {
 	public static String dividibleBy(int number) {
 		if (number % 15 == 0) return "fizzbuzz";
 		else if (number % 5 == 0) return "buzz";
+		else if (number % 3 == 0) return "fizz";
 		return Integer.toString(number); 
 	}
 }
