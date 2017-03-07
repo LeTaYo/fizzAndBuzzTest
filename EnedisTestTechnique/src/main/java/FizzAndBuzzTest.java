30be8f18a959796623fef4d93cad800f65ae5b13
younes <younes@192.168.1.34>
2017-03-07 20:19:53 +0100

    feat(test): add the test when the number is divided by 15

--- EnedisTestTechnique/src/main/java/FizzAndBuzzTest.java
+++ EnedisTestTechnique/src/main/java/FizzAndBuzzTest.java
@@ -1,3 +1,4 @@
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -6,10 +7,14 @@ public class FizzAndBuzzTest {
 	@Test   
 	public void testNotDevidibleByAnyOf(){     
 		Assert.assertEquals("1", FizzAndBuzz.dividibleBy(1));
-	    Assert.assertEquals("2", FizzAndBuzz.dividibleBy(2));
-	    
+	    Assert.assertEquals("2", FizzAndBuzz.dividibleBy(2));    
 	}
-
+	
+	@Test
+	 public void testDevidibleBy15() {      
+		Assert.assertEquals("fizzbuzz", FizzAndBuzz.dividibleBy(15));
+	    Assert.assertEquals("fizzbuzz", FizzAndBuzz.dividibleBy(30));
+	 }
 	   
 
 }
